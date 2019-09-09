6aeacd6fd25c139c8044978319a105d770fe07c4
noelwardcd <55109740+noelwardcd@users.noreply.github.com>
2019-09-09 15:55:08 -0400

    Initial File Upload
    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# QuestionnaireResponse Parser
+
---
+++ parser.js
@@ -0,0 +1,211 @@
+// Store where the markup will be applied
+var qrSection = document.getElementById('QuestionnaireResponse');
+var qrJSONInput = document.getElementById('jsonInput');
+var qrSubmitButton = document.getElementById('submit');
+var qrDropdownBox = document.getElementById('jsonDropdown');
+
+//Initial JSON load
+const qrSampleRequestURL = 'response.json';
+const qrExampleHomecareRequestURL = 'samplehomecareref.json';
+getJSONData(qrSampleRequestURL);
+
+/* Receives a URL pointing to a JSON file either locally or on the web, parses through
+the retrived data, stores it in the textarea for editing, and updates the page to display it */
+function getJSONData(URL) {
+    var qrRequest = new XMLHttpRequest();
+    qrRequest.open('GET', URL);
+    qrRequest.responseType = 'json';
+    qrRequest.send();
+
+    /* When the JSON loads, call the function to populate the payload data onto the
+    webpage and populate the text-area with a text version of the data for testing */
+    qrRequest.onload = function() {
+        let qResponse = qrRequest.response;
+        qrJSONInput.textContent = JSON.stringify(qResponse, undefined, 3);
+        populateResponse(qResponse);
+    }
+}
+
+// Event Listener for the dropdown box
+qrDropdownBox.addEventListener('change', (event) => {
+
+    const result = event.target.value;
+
+    clearJSONResults();
+    if(result == 1) {
+        getJSONData(qrSampleRequestURL);
+    }
+    if(result == 2) {
+        getJSONData(qrExampleHomecareRequestURL);
+    }
+    
+});
+
+// Event Listener for the submit button
+qrSubmitButton.addEventListener("click", function(event) {
+
+    clearJSONResults();
+
+    // Populate the page with new data taken from the textview
+    let updateText = qrJSONInput.value;
+    let updatedJSON = JSON.parse(updateText);
+    populateResponse(updatedJSON);
+});
+
+/* Parse through the JSON file, checking for nested and conditional properties,
+then displaying all of the response contents to a section on the page */
+function populateResponse(jsonObj) {    
+    let headers = jsonObj.resource.item;
+
+    //Loop through the main categorical section headers
+    for (var indexHeader = 0; indexHeader < headers.length; indexHeader++) {
+
+        //If there is an "answer" defined at this point, a conditional question was used, indent the following items
+        if(typeof headers[indexHeader].answer !== 'undefined') {
+
+            let line = renderQuestion(headers[indexHeader]);
+            let answer = renderConditionalAnswer(headers[indexHeader].answer[0]);
+            line.appendChild(answer);
+            qrSection.appendChild(line);
+
+            //Loop the sub-questions and answer(s) to the conditional question
+            let answers = headers[indexHeader].answer;
+
+            for(var indexCondAnswer = 0; indexCondAnswer < answers.length; indexCondAnswer++) {
+                for (item in answers) {
+                    let itemText = item;
+
+                        //Loop through the associated sub-questions
+                        for(var indexSubQuestion = 0; indexSubQuestion < headers[indexHeader].answer[indexCondAnswer].item.length; indexSubQuestion++) {
+
+                            let indent = document.createElement('div');
+                            indent.style.marginLeft = '2em';
+                            let line = renderQuestion(headers[indexHeader].answer[indexCondAnswer].item[indexSubQuestion]);
+
+                            //Loop through the answers to each sub-question
+                            let answers = headers[indexHeader].answer[indexCondAnswer].item[indexSubQuestion].answer;
+                            for(var indexSubAnswer = 0; indexSubAnswer < answers.length; indexSubAnswer++) {
+
+                                let answer = renderAnswer(answers[indexSubAnswer]);
+                                line.appendChild(answer);
+
+                            }
+
+                            indent.appendChild(line);
+                            qrSection.appendChild(indent);
+                        }
+                }
+                
+            } // end of the loop to the conditional question's answer 
+
+        // Standard questions and answers will be displayed as no conditional was detected
+        } else {
+
+            let header = document.createElement('h1');
+            header.textContent = headers[indexHeader].text;
+            qrSection.appendChild(header);
+
+            //Loop through the questions and display them in bold text
+            for(var indexSubQuestion = 0; indexSubQuestion < headers[indexHeader].item.length; indexSubQuestion++) {
+
+                let line = renderQuestion(headers[indexHeader].item[indexSubQuestion]);
+                qrSection.appendChild(line);
+                
+                //jsonObj.resource.item[i].item[x].answer
+                let answers = headers[indexHeader].item[indexSubQuestion].answer;
+                for(var indexSubAnswer = 0; indexSubAnswer < answers.length; indexSubAnswer++) {
+
+                    //Loop through answers and append next to questions, multi-select are separated with commas
+                    if(answers.length > 1) {
+
+                        //On the last answer in multi-select, don't include a comma (stop at 2nd last)
+                        if(indexSubAnswer <= (answers.length-2)) {
+                        let answer = renderMultiAnswer(answers[indexSubAnswer]);
+                        line.appendChild(answer);
+                        } else { 
+                        let answer = renderAnswer(answers[indexSubAnswer]);
+                            line.appendChild(answer);
+                        }
+                        
+                    } else {
+                        //Only 1 answer was received
+                            let answer = renderAnswer(answers[indexSubAnswer])
+                            line.appendChild(answer);
+                    }
+                    
+                }
+    
+            }
+        } // end of the Else for if the headers has an "answer" defined instead of just a standard item
+        
+    } // End of the (for i = 0...) headers loop
+}
+
+/* Determines which of the associated datatypes is contained
+ in the answer object and returns the answer as a string */
+function getAnswerText(obj) {
+    let response = "";
+
+    for (item in obj) {
+        let itemText = item;
+        response += obj[itemText];
+    }
+
+    return response;
+}
+
+/* Displays a question on the page by taking an object, creating a paragraph element,
+styling it bold with a span, and then returning the question paragraph as an object,
+verifies whether the last character of the question is a semi-colon, if not one is added */
+function renderQuestion(obj) {
+    let line = document.createElement('p');
+    let question = document.createElement('span');
+    question.style.fontWeight = 'bold';
+    if(obj.text.charAt(obj.text.length-1) == ":") {
+        question.textContent = obj.text + " ";
+    } else {
+        question.textContent = obj.text + ': ';
+    }
+    line.appendChild(question);
+
+    return line;
+}
+
+/* Displays an answer on the page by taking an object, creating a span element with a
+normal style applied, to remove any bolding, and then returns the answer as an object */
+function renderAnswer(obj) {
+    let answer = document.createElement('span');
+    answer.style.fontWeight = 'normal';
+    answer.textContent = getAnswerText(obj);
+
+    return answer;
+}
+
+/* Displays a Multi-line answer on the page by taking an object, creating a span element
+with a normal style applied, to remove any bolding, adding a comma at the end, and then
+returns the answer as an object */
+function renderMultiAnswer(obj) {
+    let answer = document.createElement('span');
+    answer.style.fontWeight = 'normal';
+    answer.textContent = getAnswerText(obj) + ', ';
+
+    return answer;
+}
+
+/* Displays an answer on the page by taking an object, creating a span element with a
+normal style applied, to remove any bolding, and then returns the answer as an object,
+this should typically be used for a true/false boolean question */
+function renderConditionalAnswer(obj) {
+    let answer = document.createElement('span');
+    answer.style.fontWeight = 'normal';
+    answer.textContent = obj.valueBoolean;
+
+    return answer;
+}
+
+// Remove the previous JSON data from the page so new data can be displayed
+function clearJSONResults() {
+    while (qrSection.firstChild) {
+        qrSection.removeChild(qrSection.firstChild);
+        }
+}
